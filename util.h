c573ccea2b18df32786f8848f35054637df42b94
kvic <kvic-z@users.noreply.github.com>
2017-09-01 21:50:47 +0800

    Bump version to Kk-test3

--- util.h
+++ util.h
@@ -23,7 +23,7 @@
 #include <arpa/inet.h>
 
 // preprocessor defines
-#define VERSION "v35.HZ12.Kk-test2"
+#define VERSION "v35.HZ12.Kk-test3"
 
 #define BACKLOG SOMAXCONN       // how many pending connections queue will hold
 #define CHAR_BUF_SIZE 4095      // surprising how big requests can be with cookies and lengthy yahoo url!
@@ -96,10 +96,12 @@ extern volatile sig_atomic_t noc;
 extern volatile sig_atomic_t rdr;
 extern volatile sig_atomic_t pst;
 extern volatile sig_atomic_t hed;
+extern volatile sig_atomic_t opt;
 
 extern volatile sig_atomic_t slh;
 extern volatile sig_atomic_t slm;
 extern volatile sig_atomic_t sle;
+extern volatile sig_atomic_t slc;
 extern volatile sig_atomic_t slu;
 
 struct Global {
