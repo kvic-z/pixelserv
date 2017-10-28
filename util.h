10c435133b96940f2462345acbbcc53baf18ae09
kvic <kvic-z@users.noreply.github.com>
2017-10-29 01:35:41 +0800

    Added macro TEMP_FAILURE_RETRY for musl and LEDE. Thanks to laoshaw

--- util.h
+++ util.h
@@ -41,6 +41,16 @@
 # define DEFAULT_STATS_URL "/servstats"
 # define DEFAULT_STATS_TEXT_URL "/servstats.txt"
 
+/* taken from glibc unistd.h and fixes musl */
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression) \
+  (__extension__                                                              \
+    ({ long int __result;                                                     \
+       do __result = (long int) (expression);                                 \
+       while (__result == -1L && errno == EINTR);                             \
+       __result; }))
+#endif
+
 #ifdef TEST
 # define TESTPRINT printf
 #else
