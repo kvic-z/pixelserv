b3b1f4da8dfef2d35045416a7c42cc82f201d07b
kvic <kvic-z@users.noreply.github.com>
2017-11-28 01:03:07 +0800

    v2.0.0 release

--- util.h
+++ util.h
@@ -19,7 +19,7 @@
 #include <arpa/inet.h>
 
 // preprocessor defines
-#define VERSION "v2.0.0-rc1"
+#define VERSION "v2.0.0"
 
 #define BACKLOG SOMAXCONN       // how many pending connections queue will hold
 #define DEFAULT_IP "*"          // default IP address ALL - use this in messages only
